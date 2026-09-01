910c282247f80040cd61595ea3458bda7b26e6c5
jeanc521 <jeanromoaldoc@gmail.com>
2026-09-01 20:29:26 -0300

    Add files via upload

--- code09.php
+++ code09.php
@@ -1,3 +1,15 @@
 <?php
-    
+$peso = 75; 
+$altura = 1.75;
+$calculoIMC = $peso / ($altura * $altura);
+
+if($calculoIMC < 18.5){
+    echo "Abaixo do peso";
+}elseif($calculoIMC >= 18.5 && $calculoIMC < 25){
+    echo "Peso normal";
+}elseif($calculoIMC >= 25 && $calculoIMC < 30){
+    echo "Sobrepeso";
+}else{
+    echo "Obesidade";
+}
 ?>
